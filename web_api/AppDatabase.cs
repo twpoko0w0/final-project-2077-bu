024b7e4909246101bb7b4bb6e9c4d31a9072c35e
twpoko <tawan.hira@bumail.net>
2022-02-03 12:49:02 +0700

    [UPLOAD] First project api

---
+++ web_api/AppDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MySqlConnector;
+
+namespace web_api
+{
+    public class AppDatabase: IDisposable
+    {
+        public MySqlConnection Connection { get; }
+
+        public AppDatabase(string ConnectionString)
+        {
+            Connection = new MySqlConnection(ConnectionString);
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
